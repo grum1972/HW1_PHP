877861abdc5c5adffbb0b68f80d84750d8fbeaa1
Kotov Oleg <grum1972@gmail.com>
2020-05-31 21:39:20 +0300

    Исправил замечания наставника по ДЗ №2Ð

--- functions.php
+++ functions.php
@@ -15,17 +15,28 @@ function task1($arr, $print = false)
     return $str;
 }
 
+//task1_1
+function task1_1(array $strings, $print = false)
+{
+    $str = '';
+    if (!$print) {
+        for ($i = 0; $i < sizeof($strings); $i++) {
+            echo '<p>' . $strings[$i] . '</p>';
+        }
+    } else {
+        $str = implode('', $strings);
+    }
+    return $str;
+
+}
+
 //task2
 function task2(...$arg)
 {
-    if ($arg === '+' || $arg === '-') {
-        $result = 0;
-    } else {
-        $result = 1;
-    }
-    $output = '';
+    $result = $arg[1];
+    $output = $arg[1];
     $chr = $arg[0];
-    for ($i = 1; $i < sizeof($arg); $i++) {
+    for ($i = 2; $i < sizeof($arg); $i++) {
         switch ($arg[0]) {
             case '+':
                 $result += $arg[$i];
@@ -44,12 +55,42 @@ function task2(...$arg)
                 }
                 break;
         }
-        if ($i > 1 && $i < sizeof($arg)) {
+        if ($i < sizeof($arg)) {
             $output .= $chr . (string)$arg[$i];
         } else {
             $output .= (string)$arg[$i];
         }
     }
+    return "Результат: $output = $result";
+}
+
+// task2_1
+
+function task2_1(...$arg)
+{
+    $chr = array_shift($arg);
+    $result = $arg[0];
+    for ($i = 1; $i < sizeof($arg); $i++) {
+        switch ($chr) {
+            case '+':
+                $result += $arg[$i];
+                break;
+            case '-':
+                $result -= $arg[$i];
+                break;
+            case '*':
+                $result *= $arg[$i];
+                break;
+            case '/':
+                if ($i === 0) {
+                    $result = $arg[$i];
+                } else {
+                    $result /= $arg[$i];
+                }
+                break;
+        }
+    }
+    $output = implode($chr, $arg);
     return "Результат: $output = $result";
 }
 
@@ -67,6 +108,25 @@ function task3($arg1, $arg2)
     for ($i = 1; $i <= $arg1; $i++) {
         echo '<tr>';
         for ($j = 1; $j <= $arg2; $j++) {
+            $el = $i * $j;
+            echo "<td>$el</td>";
+        }
+    }
+    echo '</tr>';
+    echo '</table>';
+}
+
+//task3_1
+function task3_1(int $row, int $col)
+{
+    if ($row < 1 || $col < 1) {
+        echo 'Минимальный размер таблицы 1 х 1';
+        return;
+    }
+    echo '<table>';
+    for ($i = 1; $i <= $row; $i++) {
+        echo '<tr>';
+        for ($j = 1; $j <= $col; $j++) {
             $el = $i * $j;
             echo "<td>$el</td>";
         }
@@ -95,4 +155,10 @@ function task5($str, $search, $replace)
 function task6($file)
 {
     echo file_get_contents($file);
+}
+
+//task6_1
+function task6_1($file, $str)
+{
+    file_put_contents($file, $str);
 }
--- index.php
+++ index.php
@@ -5,37 +5,66 @@ $array = ["string1", "string2", "string3", "string4", "string5"];
 echo task1($array, true);
 echo '<br>';
 print_r(task1($array));
+echo task1_1($array, true);
+echo '<br><br>';
+
+
+//TASK #1_1 Use function implode()
+echo 'TASK 1 NEW VERSION' . '<br>';
+echo '<br>';
+echo task1_1($array, true);
+echo '<br><br>';
 
 // TASK #2
+echo 'TASK 2' . '<br><br>';
+
 echo task2('-', 1, 2, 3, 4, 5) . '<br>';
 echo task2('+', 1, 2, 3, 4, 5, 10.0) . '<br>';
 echo task2('*', 1, 2, 3, 4, 5) . '<br>';
 echo task2('/', 16, 2, 2, 2) . '<br>';
+echo '<br>';
+
+// TASK #2_1
+echo 'TASK2 NEW VERSION' . '<br><br>';
+
+echo task2_1('-', 1, 2, 3, 4, 5) . '<br>';
+echo task2_1('+', 1, 2, 3, 4, 5, 10.0) . '<br>';
+echo task2_1('*', 1, 2, 3, 4, 5) . '<br>';
+echo task2_1('/', 16, 2, 2, 2) . '<br>';
 
 // TASK #3
+echo 'TASK 3' . '<br>';
+
 echo '<br>';
 task3(3, 4);
 echo '<br>';
 task3(1.2, 3); // Аргумент не целое число
 echo '<br>';
 task3(0, 3); // Аргумент меньше 1
-
+echo '<br><br>';
+// TASK #3_1
+echo 'TASK 3 NEW VERSION' . '<br><br>';
+echo '<br>';
+task3_1(3, 4);
+echo '<br>';
+task3_1(0, 3); // Аргумент меньше 1
+echo '<br><br>';
 // TASK #4
-echo '<br>';
-echo '<br>';
+echo 'TASK 4' . '<br>';
+echo '<br><br>';
 task4();
 
 // TASK #5
-echo '<br>';
-echo '<br>';
+echo 'TASK 5' . '<br>';
+echo '<br><br>';
 echo task5('Карл у Клары украл Кораллы', 'К', '');
 echo '<br>';
 echo task5('Две бутылки лимонада', 'Две', 'Три');
 
 //TASK #6
-echo '<br>';
-echo '<br>';
+echo 'TASK 6' . '<br>';
+echo '<br><br>';
 $file = 'test.txt';
 $str = 'Hello again!';
-file_put_contents($file,$str);
+task6_1($file,$str);
 task6($file);
